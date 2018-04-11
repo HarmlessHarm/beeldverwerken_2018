3c23c4bc93612402bfcb64ea755b21c416967bb0
Harm Manders <harmmanders@gmail.com>
2018-04-11 16:47:45 +0200

    started rotateImage

--- Lab1/linear.m
+++ Lab1/linear.m
@@ -2,13 +2,15 @@ function color = linear(image, x, y)
 
     a = x - floor(x);
     b = y - floor(y);
+    size(a)
+    size(b)
     k = floor(x);
     l = floor(y);
     
-    kl   = (1 - a) * (1 - b) * image(k, l);
-    kl1  = (1 - a) * b * image(k , l + 1);
-    k1l1  = a * b * image(k + 1, l + 1);
-    k1l = a * (1 - b) * image(k + 1, l);
+    kl   = (1 - a) .* (1 - b) .* image(k, l);
+    kl1  = (1 - a) .* b .* image(k , l + 1);
+    k1l1  = a .* b .* image(k + 1, l + 1);
+    k1l = a .* (1 - b) .* image(k + 1, l);
     
     color = kl + kl1 + k1l1 + k1l;
     return;
