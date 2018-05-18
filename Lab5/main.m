22d67d3ebdd12a5d40952e6ed36124f8762d6472
Harm Manders <harmmanders@gmail.com>
2018-05-18 13:08:16 +0200

    lalala

--- Lab5/main.m
+++ Lab5/main.m
@@ -22,12 +22,14 @@ for i = 1:train
 	pos_train(:,i) = s.position';
 end
 X_test = zeros(numel(images{1}.img),length(images)-train);
+size(X_test)
 pos_test = zeros(2,length(images)-train);
 for i = train+1:length(images)
     s = images{i};
-    X_test(:,i) = reshape(s.img,numel(s.img),1);
+    X_test(:,i-train) = reshape(s.img,numel(s.img),1);
 	pos_test(:,i) = s.position';
 end
+size(X_test)
 
 %% Apply PCA to train set
 
@@ -55,7 +57,7 @@ Eigenvals = sum(S(:,1:150)).^2;
 plot(Eigenvals)
 
 %%
-k = 25;
+k = 250;
 
 Scores = U(:,1:k)' * X_train;
 Images = U(:,1:k) * Scores;
@@ -96,5 +98,32 @@ round(img_t / N, 5)
 % You see a dip in distance to 0 at own index.
 
 
+%% Positioning with Nearest Neighbour
 
+pcaTrain = U(:,1:k)' * X_train;
+pcaTest = U(:,1:k)' * X_test;
+
+nearestCoordinates = zeros(2,length(pcaTest));
+
+for i = 1:length(pcaTest)
+    
+   dist = sqrt(sum((pcaTrain - pcaTest(:,i)).^2));
+   minimum_dist = min(dist);
+   trainInd = find(dist == minimum_dist);
+   coords = images{trainInd}.position;
+   nearestCoordinates(:,i) = coords';
+    
+end
+
+figure(7)
+imagestruct = [images{:}];
+positions = vertcat(imagestruct.position)';
+scatter(positions(1,1:train), positions(2,1:train), 'r')
+hold on
+scatter(nearestCoordinates(1,:), nearestCoordinates(2,:), 'b')
+scatter(positions(1,train+1:end), positions(2,train+1:end), 'g')
+hold off
+
+euclidean = sqrt(sum((nearestCoordinates - positions(:,train+1:end)).^2));
+sum(euclidean > 150)
 
